a629872b1f27592242fc05402ca5c24ce2d8cd67
Perennate <perennate@gmail.com>
2011-07-21 23:50:08 +0000

    Added copycat plugin.

---
+++ plugins/python/copycat.py
@@ -0,0 +1,36 @@
+# author = uakf.b
+# version = 1.0
+# name = copycat
+# fullname = plugins/pychop/copycat
+# description = Copies everything that users say in the channel.
+# help = !copycat to toggle copycat.
+
+# modify settings below
+
+# commands to trigger on
+commands = ["copycat", "plugins/pychop/copycat"]
+
+# end settings
+
+# whether or not copycat is enabled
+copycatEnabled = False
+
+import host
+
+def init():
+	host.registerHandler('ProcessCommand', onCommand)
+	host.registerHandler('ChatReceivedExtended', onTalk) # extended to distinguish between local chat and whispers
+	
+def deinit():
+	host.unregisterHandler(onCommand)
+	host.unregisterHandler(onTalk)
+
+def onTalk(bnet, username, message, isWhisper):
+	if not isWhisper and copycatEnabled:
+		bnet.queueChatCommand(message)
+
+def onCommand(bnet, user, command, payload, nType):
+	global copycatEnabled
+	
+	if command in commands:
+		copycatEnabled = not copycatEnabled
