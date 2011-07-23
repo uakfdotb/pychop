7edd52ed6dcbb3cb3fcb843c28159008cf2933ec
perennate@gmail.com <perennate@gmail.com@a524e15b-62e1-5cbc-1d36-ca87b18a2cf5>
2011-07-23 13:03:13 +0000

    Added two new untested plugins: announce mimics GHost++ !announce command, except for channel use, and votekick votekicks users from the channel (multiple votekicks at one time are supported).

---
+++ trunk/plugins/python/announce.py
@@ -0,0 +1,58 @@
+# author = uakf.b
+# version = 1.0
+# name = announce
+# fullname = plugins/pychop/announce
+# description = Announces a message at a set time interval.
+# help = Use !announce <x> <string> to say <string> every <x> seconds or !announce to disable announce message.
+
+### begin configuration
+
+commands = ("announce", "plugins/pychop/announce")
+
+# minimum access to control announce
+announceAccess = 10
+
+### end configuration
+
+announceEnabled = False
+announceMessage = ""
+announceInterval = 1
+announceBnet = 0
+announceTime = 0
+
+import host
+import time
+
+def init():
+	host.registerHandler('ProcessCommand', onCommand)
+	host.registerHandler('Update', onUpdate)
+	
+def deinit():
+	host.unregisterHandler(onCommand)
+	host.unregisterHandler(onUpdate)
+
+def onUpdate(chop) :
+	global announceTime
+	
+	if announceEnabled and gettime() - announceTime > announceInterval * 1000:
+		# announce
+		announceBnet.queueChatCommand(announceMessage)
+		announceTime = gettime()
+
+def gettime():
+	return int(round(time.time() * 1000))
+
+def onCommand(bnet, user, command, payload, nType):
+	global announceEnabled, announceMessage, announceInterval, announceBnet
+	announceBnet = bnet
+	
+	if command in commands and user.getAccess() > announceAccess:
+		parts = payload.split(" ", 1);
+		
+		if len(parts) < 2:
+			announceEnabled = False
+			bnet.queueChatCommand("Announce message disabled")
+		else:
+			announceInterval = int(parts[0])
+			announceMessage = parts[1]
+			announceEnabled = True
