d6f7bc7c0ab1e440457ae782227b9c4eba57fe5d
perennate@gmail.com <perennate@gmail.com@a524e15b-62e1-5cbc-1d36-ca87b18a2cf5>
2011-08-13 23:09:32 +0000

    Invite acceptance better with cookie checking.

--- trunk/chop/bnet.cpp
+++ trunk/chop/bnet.cpp
@@ -1391,10 +1391,15 @@ void CBNET :: SendClanSetMOTD( string message )
 void CBNET :: SendClanAcceptInvite( bool accept )
 {
 	if( m_LoggedIn ) {
+		BYTEARRAY SendBytes;
+		
 		if( m_LastInviteCreation )
-			m_Socket->PutBytes( m_Protocol->SEND_SID_CLANCREATIONINVITATION( accept  ));
+			SendBytes = m_Protocol->SEND_SID_CLANCREATIONINVITATION( accept );
 		else
-			m_Socket->PutBytes( m_Protocol->SEND_SID_CLANINVITATIONRESPONSE( accept ) );
+			SendBytes = m_Protocol->SEND_SID_CLANINVITATIONRESPONSE( accept );
+		
+		if( SendBytes.size( ) != 0 )
+			m_Socket->PutBytes( SendBytes );
 	}
 }
 
